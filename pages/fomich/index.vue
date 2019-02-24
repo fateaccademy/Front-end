b532c0cf91c46550c5171bacf33d547082ba0398
fomenkoyegor <fomenkoyegor@gmail.com>
2019-02-24 21:18:51 +0200

    some test vuejs

--- pages/fomich/index.vue
+++ pages/fomich/index.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
-    trulalal
+    <ul>
+      <li v-for="fruit in fruits" :key="fruit.id">
+        <h3>{{fruit.name}}</h3>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "index"
+  export default {
+    name: "index",
+    data() {
+      return {
+        fruits: [
+          {id: 1, name: "banana"},
+          {id: 2, name: "ananas"},
+          {id: 3, name: "cocos"},
+        ]
+      };
     }
+  }
 </script>
 
 <style scoped>
