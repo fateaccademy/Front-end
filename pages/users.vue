8ff12afcdd7e7f73ea424fad5f7239f94c40e283
fomenkoyegor <fomenkoyegor@gmail.com>
2019-02-26 15:52:52 +0200

    testing nuxt child

---
+++ pages/users.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>
+      this is {{name}} page
+    </h1>
+    <input type="text" v-model="userId">
+    <button @click="onLoadUser">Load User</button>
+    <nuxt-child/>
+  </div>
+
+</template>
+
+
+<script>
+  export  default {
+    data(){
+      return {
+        name:'users',
+        userId:''
+      }
+    },
+    methods:{
+      onLoadUser(){
+        this.$router.push(`/users/${this.userId}`)
+      }
+    }
+  }
+</script>
